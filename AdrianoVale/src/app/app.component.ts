b989ffdb7448a0df8fac56b146168debcf3d3644
ValeAdriano <acsvalefilho@gmail.com>
2024-04-17 16:03:37 -0300

    pz dmss

--- AdrianoVale/src/app/app.component.ts
+++ AdrianoVale/src/app/app.component.ts
@@ -4,13 +4,14 @@ import { CommonEngine } from '@angular/ssr';
 import { HeaderComponent } from './layout/header/header.component';
 import { HomeComponent } from "./home/home.component";
 import { FooterComponent } from "./layout/footer/footer.component";
+import { LoginComponent } from "./layout/login/login.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, HeaderComponent, HomeComponent, FooterComponent]
+    imports: [RouterOutlet, HeaderComponent, HomeComponent, FooterComponent, LoginComponent]
 })
 
 export class AppComponent {
